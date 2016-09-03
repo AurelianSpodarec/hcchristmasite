33847aeffb8328374aadd6213b27bed58368e48e
Aurelian Spodarec <aurelians@libero.it>
2016-09-03 20:33:25 +0100

    sound enabled

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,30 @@ $(document).ready(function(){
         $('.main-nav').slideToggle();
     });
 
+    //Desktop Menu
 
+
+    //LightBox
+    var $overlay = $('<div id="overlay"</div>');
+    var $image = $('<img>');
+
+    $overlay.append($image);
+
+    $('body').append($overlay);
+
+    $('.gallery-item img').click(function(){
+    	$(overlay).show();
+
+    	var imageLoad = $(this).attr('src');
+    	$image.attr('src', imageLoad);
+
+    });
+
+
+    $overlay.click(function(){
+    	$overlay.hide();
+    	$('#overlay .btn').hide();
+    });
    
    
 });
